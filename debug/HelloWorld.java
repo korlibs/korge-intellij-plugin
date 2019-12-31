06a6c633c885295eae08c35235c1b4c1dff7510a
soywiz <soywiz@gmail.com>
2019-12-31 01:06:09 +0100

    Provide faster way to read byte arrays from com.sun.jdi

--- debug/HelloWorld.java
+++ debug/HelloWorld.java
@@ -1,7 +1,15 @@
 import java.lang.*;
+import java.util.*;
 
+// javac -g HelloWorld.java
 public class HelloWorld {
-	static public void main(String[] args) {
+	static public void main(String[] args) throws Throwable {
+		byte[] data = new byte[128 * 1024 * 1024]; // 128 MB
+		data[0] = 1;
+		data[10000] = 2;
+		data[data.length - 1] = 3;
+		//byte[] data = new byte[64 * 1024 * 1024]; // 64 MB
 		System.out.println("HELLO");
+		//Thread.sleep(1000);
 	}
 }
