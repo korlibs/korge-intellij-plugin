8fafea4210767b19b5f623a5f029feabccf3c89e
soywiz <soywiz@gmail.com>
2020-03-05 23:10:21 +0100

    Fixes https://github.com/korlibs/korge-intellij-plugin/issues/9

--- src/main/kotlin/com/soywiz/korge/intellij/filetype/FileTypes.kt
+++ src/main/kotlin/com/soywiz/korge/intellij/filetype/FileTypes.kt
@@ -6,29 +6,91 @@ import com.intellij.openapi.fileTypes.*
 import com.soywiz.korge.intellij.*
 import javax.swing.*
 
-open class KorgeAniFileType : UserBinaryFileType() {
-	override fun getName(): String = "KORGE_ANI"
-}
-
-open class KorgeAudioFileType : UserBinaryFileType() {
-	override fun getName(): String = "KORGE_AUDIO"
-}
-
 open class XmlBaseType(
 	val _icon: Icon,
 	val _name: String,
 	val _extension: String,
-	val _description: String
-) : XmlLikeFileType(XMLLanguage.INSTANCE) {
+	val _description: String,
+	val lang: XMLLanguage
+) : XmlLikeFileType(lang) {
 	override fun getIcon(): Icon? = _icon
 	override fun getName(): String = _name
 	override fun getDefaultExtension(): String = _extension
 	override fun getDescription(): String = _description
 }
 
-open class TmxFileType : XmlBaseType(KorgeIcons.TILED, "TMX", "tmx", "Tiled Map Files")
-open class TsxFileType : XmlBaseType(KorgeIcons.TILED, "TSX", "tsx", "Tiled Tileset Files")
-open class PexFileType : XmlBaseType(KorgeIcons.PARTICLE, "PEX", "pex", "Particle Definitions")
-open class ScmlFileType : XmlBaseType(KorgeIcons.SPRITER, "SCML", "scml", "Spriter Text File")
-open class SconFileType : XmlBaseType(KorgeIcons.SPRITER, "SCON", "scon", "Spriter Binary File")
-open class FntFileType : XmlBaseType(KorgeIcons.BITMAP_FONT, "FNT", "fnt", "Font Definition File")
+open class TMXLanguage protected constructor() : XMLLanguage(XMLLanguage.INSTANCE, "KORGE_TMX", "text/tmx") {
+	companion object {
+		@JvmField val INSTANCE = TMXLanguage()
+	}
+}
+open class TSXLanguage protected constructor() : XMLLanguage(XMLLanguage.INSTANCE, "KORGE_TSX", "text/tsx") {
+	companion object {
+		@JvmField val INSTANCE = TSXLanguage()
+	}
+}
+open class PEXLanguage protected constructor() : XMLLanguage(XMLLanguage.INSTANCE, "KORGE_PEX", "text/pex") {
+	companion object {
+		@JvmField val INSTANCE = PEXLanguage()
+	}
+}
+open class SCMLLanguage protected constructor() : XMLLanguage(XMLLanguage.INSTANCE, "KORGE_SCML", "text/scml") {
+	companion object {
+		@JvmField val INSTANCE = SCMLLanguage()
+	}
+}
+open class SCONLanguage protected constructor() : XMLLanguage(XMLLanguage.INSTANCE, "KORGE_SCON", "text/scon") {
+	companion object {
+		@JvmField val INSTANCE = SCONLanguage()
+	}
+}
+open class FNTLanguage protected constructor() : XMLLanguage(XMLLanguage.INSTANCE, "KORGE_FNT", "text/fnt") {
+	companion object {
+		@JvmField val INSTANCE = FNTLanguage()
+	}
+}
+
+open class KorgeAniFileType : UserBinaryFileType() {
+	companion object {
+		@JvmField val INSTANCE = KorgeAniFileType()
+	}
+	override fun getName(): String = "KORGE_ANI"
+}
+
+open class KorgeAudioFileType : UserBinaryFileType() {
+	companion object {
+		@JvmField val INSTANCE = KorgeAudioFileType()
+	}
+	override fun getName(): String = "KORGE_AUDIO"
+}
+
+open class TmxFileType : XmlBaseType(KorgeIcons.TILED, "KORGE_TMX", "tmx", "Tiled Map Files", TMXLanguage.INSTANCE) {
+	companion object {
+		@JvmField val INSTANCE = TmxFileType()
+	}
+}
+open class TsxFileType : XmlBaseType(KorgeIcons.TILED, "KORGE_TSX", "tsx", "Tiled Tileset Files", TSXLanguage.INSTANCE) {
+	companion object {
+		@JvmField val INSTANCE = TsxFileType()
+	}
+}
+open class PexFileType : XmlBaseType(KorgeIcons.PARTICLE, "KORGE_PEX", "pex", "Particle Definitions", PEXLanguage.INSTANCE) {
+	companion object {
+		@JvmField val INSTANCE = PexFileType()
+	}
+}
+open class ScmlFileType : XmlBaseType(KorgeIcons.SPRITER, "KORGE_SCML", "scml", "Spriter Text File", SCMLLanguage.INSTANCE) {
+	companion object {
+		@JvmField val INSTANCE = ScmlFileType()
+	}
+}
+open class SconFileType : XmlBaseType(KorgeIcons.SPRITER, "KORGE_SCON", "scon", "Spriter Binary File", SCONLanguage.INSTANCE) {
+	companion object {
+		@JvmField val INSTANCE = SconFileType()
+	}
+}
+open class FntFileType : XmlBaseType(KorgeIcons.BITMAP_FONT, "KORGE_FNT", "fnt", "Font Definition File", FNTLanguage.INSTANCE) {
+	companion object {
+		@JvmField val INSTANCE = FntFileType()
+	}
+}
