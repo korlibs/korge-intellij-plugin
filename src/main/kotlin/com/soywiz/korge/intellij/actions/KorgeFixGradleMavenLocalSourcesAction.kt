3ab313af63ebb50d40378af5989d3724cbc30302
Xeno <xenotactic.game@gmail.com>
2022-09-03 17:39:22 -0700

    Add notifications. Refactoring, cleanup. Re-enable project listeners.

--- src/main/kotlin/com/soywiz/korge/intellij/actions/KorgeFixGradleMavenLocalSourcesAction.kt
+++ src/main/kotlin/com/soywiz/korge/intellij/actions/KorgeFixGradleMavenLocalSourcesAction.kt
@@ -2,20 +2,11 @@ package com.soywiz.korge.intellij.actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.roots.OrderRootType
-import com.intellij.openapi.roots.libraries.Library
-import com.intellij.openapi.roots.libraries.LibraryTablesRegistrar
-import com.intellij.openapi.vfs.LocalFileSystem
-import com.intellij.openapi.vfs.VirtualFile
-import com.soywiz.korge.intellij.util.fixLibraries
-import com.soywiz.korge.intellij.util.runWriteAction
-
-
+import com.soywiz.korge.intellij.util.LibraryFixer
 
 class KorgeFixGradleMavenLocalSourcesAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         println("Running FixGradleMavenLocalSourcesAction")
-        fixLibraries(e.project)
+        LibraryFixer.fixLibraries(e.project, true)
     }
 }
--- src/main/kotlin/com/soywiz/korge/intellij/listeners/KorgeModuleRootListener.kt
+++ src/main/kotlin/com/soywiz/korge/intellij/listeners/KorgeModuleRootListener.kt
@@ -2,30 +2,17 @@ package com.soywiz.korge.intellij.listeners
 
 import com.intellij.openapi.roots.ModuleRootEvent
 import com.intellij.openapi.roots.ModuleRootListener
-import com.soywiz.korge.intellij.util.fixLibraries
+import com.soywiz.korge.intellij.util.LibraryFixer
 import com.soywiz.korge.intellij.util.invokeLater
 import java.util.concurrent.atomic.AtomicBoolean
 
 class KorgeModuleRootListener : ModuleRootListener {
 
-    private var updateInProgress = AtomicBoolean(false)
-
     override fun beforeRootsChange(event: ModuleRootEvent) {
     }
 
     override fun rootsChanged(event: ModuleRootEvent) {
-        invokeLater {
-            println("Running KorgeGradleSyncListener")
-            if (updateInProgress.get()) {
-                println("Update currently in progress... Quick returning.")
-                return@invokeLater
-            }
-            updateInProgress.set(true)
-            try {
-                fixLibraries(event.project)
-            } finally {
-                updateInProgress.set(false)
-            }
-        }
+        println("Running KorgeGradleSyncListener")
+        LibraryFixer.fixLibraries(event.project, false)
     }
 }
