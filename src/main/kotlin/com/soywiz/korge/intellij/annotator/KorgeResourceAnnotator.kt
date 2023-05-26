58f18b04bc3880d4bf921a37d3b7c7b6299747a1
soywiz <soywiz@gmail.com>
2023-05-26 05:12:44 +0200

    Support previewing images for `resourcesVfs["path"]`

--- src/main/kotlin/com/soywiz/korge/intellij/annotator/KorgeResourceAnnotator.kt
+++ src/main/kotlin/com/soywiz/korge/intellij/annotator/KorgeResourceAnnotator.kt
@@ -15,51 +15,71 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.readBytes
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
-import com.intellij.util.ui.UIUtil
 import com.soywiz.korge.intellij.getOrPutUserData
 import com.soywiz.korge.intellij.util.fileEditorManager
 import com.soywiz.korge.intellij.util.get
 import com.soywiz.korge.intellij.util.getScaledInstance
 import com.soywiz.korge.intellij.util.rootFile
 import korlibs.image.awt.toBufferedImage
-import org.jetbrains.kotlin.psi.KtAnnotationEntry
-import org.jetbrains.kotlin.psi.KtStringTemplateExpression
-import org.jetbrains.plugins.notebooks.visualization.use
-import java.awt.RenderingHints
+import org.jetbrains.kotlin.psi.KtArrayAccessExpression
+import org.jetbrains.kotlin.psi.psiUtil.referenceExpression
 import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
 import javax.swing.Icon
 import javax.swing.ImageIcon
 
 
-class KorgeTypedResourceAnnotator : Annotator {
+class KorgeResourceAnnotator : Annotator {
+    companion object {
+        fun extractResourcesVfsPath(element: PsiElement): String? {
+            if (element !is KtArrayAccessExpression) return null
+            //println("KorgeResourceAnnotator.extractResourcesVfsPath: $element : ${element.text}")
+            val refText = element.referenceExpression()?.firstChild?.text ?: return null
+            //println(" -> $refText")
+            if (refText != "resourcesVfs") return null
+            val expression = element.indexExpressions.firstOrNull() ?: return null
+            //println(" -> $expression")
+            val expressionText = expression.text
+            //println(" -> $expressionText")
+            return expressionText.trim('"')
+        }
+    }
+
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
-        if (element !is KtAnnotationEntry) return
-        val firstExpression = element.valueArguments.firstOrNull()?.getArgumentExpression()
-        if (firstExpression !is KtStringTemplateExpression) return
-        val annotationText = element.calleeExpression?.node?.text
-        if (annotationText != "ResourceVfsPath") return
-        val resourcePath = firstExpression.node.text.trim('"')
+        val resourcePath = KorgeResourceAnnotator.extractResourcesVfsPath(element) ?: return
         if (resourcePath.endsWith(".png")) {
             holder.addOpenImageAnnotation(element.project, resourcePath)
         }
+
+        return // @TODO: This shouldn't be necessary anymore, as KorgeTypedResourceExAnnotator is providing already the icons in both cases
+        //if (element !is KtAnnotationEntry) return
+        //val firstExpression = element.valueArguments.firstOrNull()?.getArgumentExpression()
+        //if (firstExpression !is KtStringTemplateExpression) return
+        //val annotationText = element.calleeExpression?.node?.text
+        //if (annotationText != "ResourceVfsPath") return
+        //val resourcePath = firstExpression.node.text.trim('"')
+        //if (resourcePath.endsWith(".png")) {
+        //    holder.addOpenImageAnnotation(element.project, resourcePath)
+        //}
     }
 }
 
 fun AnnotationHolder.addOpenImageAnnotation(project: Project, resourcePath: String) {
     val holder = this
-    val virtualFile = project.getResourceVirtualFile(resourcePath)
+    val virtualFile = project.getResourceVirtualFile(resourcePath) ?: return
+    if (!virtualFile.exists()) return
+
     holder.newAnnotation(HighlightSeverity.INFORMATION, "Open image")
         .withFix(object : BaseIntentionAction() {
             override fun getText(): String = "Open $resourcePath"
             override fun getFamilyName(): String = text
             override fun isAvailable(project: Project, editor: Editor?, file: PsiFile?): Boolean = true
             override fun invoke(project: Project, editor: Editor?, file: PsiFile?) {
-                OpenVirtualFileAction(virtualFile!!).openFile(project)
+                OpenVirtualFileAction(virtualFile).openFile(project)
             }
         })
         .gutterIconRenderer(
-            DefaultGutterIconRenderer(OpenVirtualFileAction(virtualFile!!),
+            DefaultGutterIconRenderer(OpenVirtualFileAction(virtualFile),
                 ImageIcon(project.getCachedResourceIcon(resourcePath))
             ))
         .create()
