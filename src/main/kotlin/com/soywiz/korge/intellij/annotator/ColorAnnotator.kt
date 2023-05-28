a4ef6423fadcb43d3bd1578724c4bf70499a1341
soywiz <soywiz@gmail.com>
2023-05-28 23:04:20 +0200

    Support MaterialColors + show color documentation

--- src/main/kotlin/com/soywiz/korge/intellij/annotator/ColorAnnotator.kt
+++ src/main/kotlin/com/soywiz/korge/intellij/annotator/ColorAnnotator.kt
@@ -8,95 +8,144 @@ import com.intellij.openapi.editor.markup.*
 import com.intellij.openapi.project.*
 import com.intellij.psi.*
 import com.intellij.psi.util.*
-import com.intellij.ui.*
 import com.intellij.util.ui.*
 import korlibs.datastructure.*
 import com.soywiz.korge.intellij.resolver.*
 import com.soywiz.korge.intellij.util.*
 import korlibs.image.color.*
 import korlibs.image.color.Colors
+import org.jetbrains.kotlin.idea.core.resolveType
 import org.jetbrains.kotlin.psi.*
 import java.awt.*
 import javax.swing.*
+import kotlin.reflect.full.memberProperties
 
 class ColorAnnotator: Annotator {
-    override fun annotate(element: PsiElement, holder: AnnotationHolder) {
-        if (element !is KtElement) return
-
-        val leftExpression = when (element) {
-            is KtDotQualifiedExpression -> element.receiverExpression
-            is KtArrayAccessExpression -> element.arrayExpression
-            else -> return
-        }
-        val leftExpressionText = leftExpression?.text
-
-        if (leftExpressionText != Colors::class.simpleName && leftExpressionText != Colors::class.qualifiedName) {
-            return
+    companion object {
+        private val materialColors: Map<String, RGBA?> = MaterialColors::class.memberProperties.toList().associate {
+            it.name to (it.getValue(MaterialColors, it) as? RGBA?)
         }
 
-        fun gutter(rgba: RGBA) {
-            val color = rgba.toAwt()
-            val renderer = GutterColorRenderer(element, color)
-            holder.newAnnotation(HighlightSeverity.INFORMATION, "Color")
-                .withFix(object : BaseIntentionAction() {
-                    override fun getText(): String = "Choose color..."
-                    override fun getFamilyName(): String = text
-                    override fun isAvailable(project: Project, editor: Editor?, file: PsiFile?): Boolean = true
-                    override fun invoke(project: Project, editor: Editor?, file: PsiFile?) = renderer.chooseColor()
-                })
-                .gutterIconRenderer(renderer)
-                .create()
+        fun colorNameToRGBA(name: String?): RGBA? = try {
+            //println("colorNameToRGBA:name=$name, materialColors=$materialColors")
+            materialColors[name] ?: (if (name != null) Colors[name] else null)
+        } catch (e: Throwable) {
+            null
         }
 
-        val context by lazy { KorgeTypeResolver(element) }
+        fun tryGetColorFromElement(element: PsiElement): RGBA? {
+            if (element is KtProperty) {
+                val rtype = element.typeReference?.text
+                //val rtype = if (element.typeReference?.text == null) {
+                //    val context by lazy { KorgeTypeResolver(element) }
+                //    context.getFqName(element)
+                //} else {
+                //    element.typeReference?.text
+                //}
 
-        //val annotationSessions = holder.currentAnnotationSession
 
-        when (element) {
-            is KtDotQualifiedExpression -> {
-                val receiverExpression = element.receiverExpression
-                val selectorExpression = element.selectorExpression
-                val typeSelector by lazy { context.getFqName(selectorExpression) }
-                val typeReceiver by lazy { context.getFqName(receiverExpression) }
+                //println("${element.text} == type=${rtype}")
 
-                if (typeReceiver == Colors::class.java.name && typeSelector == RGBA::class.java.name) {
-                    if (selectorExpression is KtNameReferenceExpression) {
-                        try {
-                            gutter(Colors[selectorExpression.getReferencedName()])
-                        } catch (e: Throwable) {
-                            e.printStackTrace()
+
+                if (rtype == "korlibs.image.color.RGBA") {
+                    try {
+                        return colorNameToRGBA(element.name)
+                    } catch (e: Throwable) {
+
+                    }
+                }
+
+            }
+
+            if (element !is KtElement) return null
+
+            val leftExpression = when (element) {
+                is KtDotQualifiedExpression -> element.receiverExpression
+                is KtArrayAccessExpression -> element.arrayExpression
+                else -> return null
+            }
+            val leftExpressionText = leftExpression?.text
+
+            if (
+                leftExpressionText != Colors::class.simpleName
+                && leftExpressionText != Colors::class.qualifiedName
+                && leftExpressionText != MaterialColors::class.simpleName
+                && leftExpressionText != MaterialColors::class.qualifiedName
+                ) {
+                return null
+            }
+
+            val context by lazy { KorgeTypeResolver(element) }
+
+            //val annotationSessions = holder.currentAnnotationSession
+
+            when (element) {
+                is KtDotQualifiedExpression -> {
+                    val receiverExpression = element.receiverExpression
+                    val selectorExpression = element.selectorExpression
+                    val typeSelector by lazy { context.getFqName(selectorExpression) }
+                    val typeReceiver by lazy { context.getFqName(receiverExpression) }
+
+                    if (
+                        typeSelector == RGBA::class.java.name &&
+                        (typeReceiver == Colors::class.java.name || typeReceiver == MaterialColors::class.java.name)
+                    ) {
+                        if (selectorExpression is KtNameReferenceExpression) {
+                            try {
+                                return colorNameToRGBA(selectorExpression.getReferencedName())
+                                //gutter()
+                            } catch (e: Throwable) {
+                                e.printStackTrace()
+                            }
                         }
                     }
                 }
-            }
-            is KtArrayAccessExpression -> {
-                //println("KtArrayAccessExpression[0]")
-                if (element.indexExpressions.size == 1) {
-                    //println("KtArrayAccessExpression[1]")
-                    val indexExpression = element.indexExpressions.firstOrNull()
-                    if (indexExpression is KtStringTemplateExpression && !indexExpression.hasInterpolation()) {
-                        //println("KtArrayAccessExpression[2]")
-                        val indexEntries = indexExpression.entries
-                        if (indexEntries.size == 1 && indexEntries[0] is KtLiteralStringTemplateEntry) {
-                            //println("KtArrayAccessExpression[3]")
-                            val indexEntry = indexEntries[0] as KtLiteralStringTemplateEntry
-                            val indexText = indexEntry.text
-                            val arrayExpression = element.arrayExpression
-                            val typeArray = context.getFqName(arrayExpression)
-                            //println("KtArrayAccessExpression[4]: $typeArray")
-                            if (typeArray == Colors::class.java.name) {
-                                //println("KtArrayAccessExpression[5]")
-                                try {
-                                    gutter(Colors[indexText])
-                                } catch (e: Throwable) {
-                                    e.printStackTrace()
+                is KtArrayAccessExpression -> {
+                    //println("KtArrayAccessExpression[0]")
+                    if (element.indexExpressions.size == 1) {
+                        //println("KtArrayAccessExpression[1]")
+                        val indexExpression = element.indexExpressions.firstOrNull()
+                        if (indexExpression is KtStringTemplateExpression && !indexExpression.hasInterpolation()) {
+                            //println("KtArrayAccessExpression[2]")
+                            val indexEntries = indexExpression.entries
+                            if (indexEntries.size == 1 && indexEntries[0] is KtLiteralStringTemplateEntry) {
+                                //println("KtArrayAccessExpression[3]")
+                                val indexEntry = indexEntries[0] as KtLiteralStringTemplateEntry
+                                val indexText = indexEntry.text
+                                val arrayExpression = element.arrayExpression
+                                val typeArray = context.getFqName(arrayExpression)
+                                //println("KtArrayAccessExpression[4]: $typeArray")
+                                if (typeArray == Colors::class.java.name) {
+                                    //println("KtArrayAccessExpression[5]")
+                                    try {
+                                        return colorNameToRGBA(indexText)
+                                    } catch (e: Throwable) {
+                                        e.printStackTrace()
+                                    }
                                 }
                             }
                         }
                     }
                 }
             }
+            return null
         }
+    }
+
+    override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+        val rgba = tryGetColorFromElement(element) ?: return
+
+        val color = rgba.toAwt()
+        val renderer = GutterColorRenderer(element, color)
+        holder.newAnnotation(HighlightSeverity.INFORMATION, "Color")
+            .withFix(object : BaseIntentionAction() {
+                override fun getText(): String = "Choose color..."
+                override fun getFamilyName(): String = text
+                override fun isAvailable(project: Project, editor: Editor?, file: PsiFile?): Boolean = true
+                override fun invoke(project: Project, editor: Editor?, file: PsiFile?) = renderer.chooseColor()
+            })
+            .gutterIconRenderer(renderer)
+            .create()
     }
 }
 
@@ -113,7 +162,7 @@ data class GutterColorRenderer(val element: PsiElement, val color: Color): Gutte
             val rgba = newColor.toRgba()
             val colorName = COLORS_TO_NAMES[rgba]
             val replacement = when {
-                colorName != null -> "Colors.${colorName.toUpperCase()}"
+                colorName != null -> "Colors.${colorName.uppercase()}"
                 rgba.a == 0xFF -> "Colors[\"${rgba.hexStringNoAlpha}\"]"
                 else -> "Colors[\"${rgba.hexString}\"]"
             }
--- src/main/kotlin/com/soywiz/korge/intellij/documentation/KorgePsiDocumentationTargetProvider.kt
+++ src/main/kotlin/com/soywiz/korge/intellij/documentation/KorgePsiDocumentationTargetProvider.kt
@@ -1,17 +1,22 @@
 package com.soywiz.korge.intellij.documentation
 
 import com.intellij.model.Pointer
+import com.intellij.platform.backend.documentation.DocumentationContent
 import com.intellij.platform.backend.documentation.DocumentationResult
 import com.intellij.platform.backend.documentation.DocumentationTarget
 import com.intellij.platform.backend.documentation.PsiDocumentationTargetProvider
 import com.intellij.platform.backend.presentation.TargetPresentation
 import com.intellij.psi.PsiElement
+import com.soywiz.korge.intellij.annotator.ColorAnnotator
 import com.soywiz.korge.intellij.annotator.KorgeTypedResourceExAnnotator
 import com.soywiz.korge.intellij.annotator.getResourceVirtualFile
+import korlibs.image.color.RGBA
+import korlibs.image.color.toAwt
 import korlibs.math.geom.ScaleMode
 import korlibs.math.geom.SizeInt
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.awt.image.BufferedImage
 import java.io.File
 import java.io.IOException
 import javax.imageio.ImageIO
@@ -27,24 +32,57 @@ fun isPathForImage(path: String): Boolean {
 class KorgePsiDocumentationTargetProvider : PsiDocumentationTargetProvider {
     override fun documentationTarget(element: PsiElement, originalElement: PsiElement?): DocumentationTarget? {
         //println("KorgePsiDocumentationTargetProvider.documentationTarget: $element, ${element.text}")
-        val resourcePath = KorgeTypedResourceExAnnotator.getElementResourcesVfsPath(element) ?: return null
-        //println(" -> $resourcePath")
-        if (!isPathForImage(resourcePath)) return null
-        val virtualFile = element.project.getResourceVirtualFile(resourcePath) ?: return null
-        //println(" -> $virtualFile")
-        val file = virtualFile.toNioPath().toFile()
-        //println(" -> $file")
-        return HTMLDocumentationTarget(file, resourcePath)
+        KorgeTypedResourceExAnnotator.getElementResourcesVfsPath(element)?.let { resourcePath ->
+            //println(" -> $resourcePath")
+            if (!isPathForImage(resourcePath)) return null
+            val virtualFile = element.project.getResourceVirtualFile(resourcePath) ?: return null
+            //println(" -> $virtualFile")
+            val file = virtualFile.toNioPath().toFile()
+            //println(" -> $file")
+            return HTMLDocumentationTarget(file, resourcePath)
+        }
+
+        //println("$element : ${element::class.java}")
+        ColorAnnotator.tryGetColorFromElement(element)?.let { rgba ->
+            return ColorDocumentationTarget(rgba)
+        }
+
+        return null
+    }
+
+    abstract class BaseDocumentationTarget : DocumentationTarget, Pointer<BaseDocumentationTarget> {
+        override fun createPointer(): Pointer<out DocumentationTarget> = this
+        override fun dereference(): BaseDocumentationTarget? = this
+    }
+
+    class ColorDocumentationTarget(
+        val color: RGBA
+    ) : BaseDocumentationTarget() {
+        override fun computePresentation(): TargetPresentation =
+            TargetPresentation.builder(color.hexString).presentation()
+
+        override fun computeDocumentation(): DocumentationResult? =
+            DocumentationResult.documentation(DocumentationContent.content(
+                """
+                    <div>
+                        <img src="https://127.0.0.1/image.png" width=140 height=140 />
+                        <p>${color.hexString}</p>
+                    </div>
+                """.trimIndent(),
+                mutableMapOf(
+                    "https://127.0.0.1/image.png" to BufferedImage(1, 1, BufferedImage.TYPE_4BYTE_ABGR).also {
+                        it.setRGB(0, 0, color.toAwt().rgb)
+                    }
+                )
+            ))
     }
 
     class HTMLDocumentationTarget(
         val file: File,
         val resourcePath: String
-    ) : DocumentationTarget, Pointer<HTMLDocumentationTarget> {
+    ) : BaseDocumentationTarget() {
         override fun computePresentation(): TargetPresentation =
             TargetPresentation.builder("${file.name}").presentation()
-
-        override fun createPointer(): Pointer<out DocumentationTarget> = this
 
         override fun computeDocumentation(): DocumentationResult? =
             DocumentationResult.asyncDocumentation {
@@ -63,8 +101,6 @@ class KorgePsiDocumentationTargetProvider : PsiDocumentationTargetProvider {
                     """.trimIndent()
                 )
             }
-
-        override fun dereference(): HTMLDocumentationTarget? = this
     }
 }
 
