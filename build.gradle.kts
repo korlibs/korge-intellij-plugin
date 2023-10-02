7d981e18b63c50ddb7afa32bf8d197eca6fc6e14
Carlos Ballesteros Velasco <soywiz@gmail.com>
2023-10-02 19:21:42 +0200

    Try to avoid null pointers in global private settings (#49)

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,8 @@ buildscript {
 plugins {
     java
     idea
-    id("org.jetbrains.intellij") version "1.13.3"
+    id("org.jetbrains.intellij") version "1.15.0"
+    //id("org.jetbrains.intellij") version "1.13.3"
 //    id("org.jetbrains.intellij") version "1.8.1"
 //    id("org.jetbrains.intellij") version "1.7.0"
     //id("org.jetbrains.intellij") version "1.5.3" //
